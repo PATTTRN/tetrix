2d17104d5e7e6d150b44ffcf2a2fd18c5dc20d3d
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2024-10-26 16:15:04 +0100

    initiating backend

---
+++ tetrix-backend/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let moves : String = "L".to_string() ;
+    let score : i32 = 100;
+
+
+    println!("Hello, world!");
+}
+
---
+++ tetrix-backend/target/debug/deps/tetrix_backend-452ae843511d473b.d
@@ -0,0 +1,5 @@
+/Users/wisemrmusa/code/projects/patttrn/tetrix/tetrix/tetrix-backend/target/debug/deps/libtetrix_backend-452ae843511d473b.rmeta: src/main.rs
+
+/Users/wisemrmusa/code/projects/patttrn/tetrix/tetrix/tetrix-backend/target/debug/deps/tetrix_backend-452ae843511d473b.d: src/main.rs
+
+src/main.rs:
